136e05c28f1a08a3600b766d80443f1bca3a190f
namanh2323 <101866057+namanh2310@users.noreply.github.com>
2022-06-20 21:28:05 +0700

    first-commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const video = document.querySelector("#videoEl")
+// const btn = document.querySelector('.btn')
+
+// let isAllow = false
+
+// btn.addEventListener('click', (e) => {
+//     isAllow = !isAllow
+//     if(isAllow) {
+//         video.classList.add('active')   
+//     } else {
+//         video.classList.remove('active')   
+//     }
+// })
+
+const loadFaceAPI =  async () => {
+    await faceapi.nets.faceLandmark68Net.loadFromUri('/models')
+    await faceapi.nets.faceRecognitionNet.loadFromUri('./models')
+    await faceapi.nets.tinyFaceDetector.loadFromUri('./models')
+    await faceapi.nets.faceExpressionNet.loadFromUri('./models')
+}
+
+function getCameraStream() {
+    if(navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia( { video: {} })
+            .then(stream => {
+                video.srcObject = stream
+            })
+    }
+}
+
+video.addEventListener('playing', () => {
+    const canvas = faceapi.createCanvasFromMedia(video)
+    document.body.append(canvas)
+    const displaySize = {
+        width: video.videoWidth,
+        height: video.videoHeight
+    }
+
+    setInterval(async() => {
+        const detects = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+            .withFaceLandmarks()
+            .withFaceExpressions()
+        
+        const resizedDetects = faceapi.resizeResults(detects, displaySize)
+        canvas.getContext('2d').clearRect( 0, 0, displaySize.width, displaySize.height)
+        faceapi.draw.drawDetections(canvas, resizedDetects)
+        faceapi.draw.drawFaceLandmarks(canvas, resizedDetects)
+        faceapi.draw.drawFaceExpressions(canvas, resizedDetects)
+    }, 300)
+})
+
+loadFaceAPI()
+    .then(getCameraStream())
